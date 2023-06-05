4380668d9883037e5343f2e226ac55fb636a468d
Made-Budhi <madebudhi15@gmail.com>
2023-06-05 08:01:37 +0800

    menyelesaikan tugas bootstrap

--- kelas-2c/2215354023/index.php
+++ kelas-2c/2215354023/index.php
@@ -6,6 +6,7 @@
 	<title>Punya Budhi</title>
 
     <link rel="stylesheet" href="nav-style.css">
+    <link rel="stylesheet" href="main-style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
     <style>
@@ -13,14 +14,19 @@
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
             margin: 0;
             padding: 0;
+            box-sizing: border-box;
+        }
+
+        html {
+            overflow-x: hidden;
         }
     </style>
 </head>
 <body>
 
-    <div class="container-fluid">
+    <div class="bg-light">
 
-        <div class="top-nav bg-light">
+        <div class="top-nav">
             
             <div class="logo col-6">
                 <img src="https://ecs7.tokopedia.net/assets-tokopedia-lite/v2/zeus/production/e5b8438b.svg" width="250px" alt="">
@@ -42,24 +48,105 @@
     
         </div>
     
+    </div>
+
+    <div class="container-fluid">
+
+
         <nav class="mt-3 p-4 bg-light">
     
             <ul>
-                <a href="">
+                <a class="text-decoration-none" href="">
                     <li>HOME</li>
                 </a>
-                <a href="">
+                <a class="text-decoration-none" href="">
                     <Li>SHOP</Li>
                 </a>
-                <a href="">
+                <a class="text-decoration-none" href="">
                     <Li>BLOG</Li>
                 </a>
-                <a href="">
+                <a class="text-decoration-none" href="">
                     <Li>ABOUT US</Li>
                 </a>
             </ul>
     
         </nav>
+
+        <!-- main section -->
+        <main class="mt-3">
+
+            <div class="section col-3 bg-light">
+
+                <ul class="">
+                    <li>Section 1</li>
+                    <li>Section 2</li>
+                    <li>Section 3</li>
+                    <li>Section 4</li>
+                    <li>Section 5</li>
+                    <li>Section 6</li>
+                    <li>Section 7</li>
+                    <li>Section 8</li>
+                    <li>Section 9</li>
+                    <li>Section 10</li>
+                    <li>Section 11</li>
+                    <li>Section 12</li>
+                    <li>Section 13</li>
+                    <li>Section 14</li>
+                    <li>Section 15</li>
+                </ul>
+
+            </div>
+
+            <div class="col-6">
+
+                <div class="image">
+                    <img src="./BudhiSwag-removebg-preview.png" alt="">
+                </div>
+
+                <div class="row items">
+
+                    <div class="col-4">
+                        <img src="https://images.tokopedia.net/img/cache/300-square/VqbcmM/2023/3/10/2894fdc7-25d3-430b-b986-62b8ba53e2db.jpg" alt="">
+
+                    </div>
+
+                    <div class="col-4">
+                        <img src="https://images.tokopedia.net/img/cache/300-square/VqbcmM/2023/3/10/072feb2e-b023-4961-bc16-86c465c060c2.jpg" alt="">
+                    </div>
+
+                    <div class="col-4">
+                        <img src="https://images.tokopedia.net/img/cache/300-square/VqbcmM/2022/3/3/16228258-fab1-4ea9-88c8-f632af4b7c5d.jpg" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="feature col-3">
+
+                <div class="feature-list bg-light">
+
+                    <ul>
+                        <li>Feature 1</li>
+                        <li>Feature 2</li>
+                        <li>Feature 3</li>
+                        <li>Feature 4</li>
+                        <li>Feature 5</li>
+                        <li>Feature 6</li>
+                    </ul>
+
+                </div>
+
+
+                <div class="link col-12 bg-light">
+
+                    <a href="" class="text-decoration-none">External Links</a>
+
+                </div>
+
+            </div>
+
+        </main>
 
     </div>
 
