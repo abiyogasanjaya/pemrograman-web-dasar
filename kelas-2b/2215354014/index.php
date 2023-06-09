1540189a341a0cb6bed5786739d774410e7391ee
dymirahanjni <dymirahanjani@gmail.com>
2023-06-09 18:31:29 +0800

    js2

--- kelas-2b/2215354014/index.php
+++ kelas-2b/2215354014/index.php
@@ -1,47 +1,79 @@
-<html>
-<head>
-    <title> latihan 1</title>
-</head>
-<body>
-    <b>Tabel dan Gambar di HTML dayu mirah</b>
-    <hr/>
-    <b>Membuat Tabel</b> <!-- Cara buat tabel -->
-    <br/>
-    <table border="1" width="100%">
-        <tr>
-            <th>No</th>
-            <th>NIM</th>
-            <th>Nama Mahasiswa </th>
-            <th>Prodi</th>
-        </tr>
-        <tr>
-            <td align="center">1</td>
-            <td align="center">020196</td>
-            <td>Kim Doyoung </td>
-            <td align="center">NCT 127</td>
-        </tr>
-        <tr>
-            <td align="center">2</td>
-            <td align="center">130800</td>
-            <td> Na Jaemin </td>
-            <td align="center">NCT Dream</td>
-        </tr>
-        <tr>
-            <td colspan="3" align="center"> Jumlah Mahasiswa </td>
-            <td align="center"> 2 Orang</td>
-        </tr>
-    </table>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <hr/>
-    <b>Gambar</b> <br/> <!-- Cara buat gambar -->
-    <marquee>
-    <img src="doyoung.jpeg" height="200" width="320"/>
-    </marquee>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <hr/>
-    <b>Google Maps :</b> <!-- Cara buat google maps -->
-    <br/>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3942.861183032755!2d115.1594973140249!3d-8.79910989238934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd244c13ee9d753%3A0x6c05042449b50f81!2sPoliteknik%20Negeri%20Bali!5e0!3m2!1sid!2sid!4v1679272627035!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <title></title>
+  </head>
+  <body>
+      <ul class="nav justify-content-end bg-success">
+        <li class="nav-item">
+          <a class="nav-link active text-white" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled text-white">Disabled</a>
+        </li>
+      </ul>
+      <nav class="navbar navbar-expand-lg navbar-light bg-success">
+        <a class="navbar-brand" href="#"><img src="" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+      <h1></h1>
+    </div>
 
-</body>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
 </html>
